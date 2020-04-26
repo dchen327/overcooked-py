3381869b02acff60512094690c91f1e04eb01e8a
David Chen <davidc3287@gmail.com>
2020-04-26 01:13:36 -0400

    add directions, support for diagonal movement but very inaccurate at the moment

--- overcooked_movement_test.py
+++ overcooked_movement_test.py
@@ -1,6 +1,19 @@
 import pyautogui
 from time import sleep
 
+pyautogui.PAUSE = 0  # no delay between clicks
+
+directions = {
+    'N': 'up',
+    'E': 'right',
+    'S': 'down',
+    'W': 'left',
+    'NE': 'up,right',
+    'SE': 'down,right',
+    'SW': 'down,left',
+    'NW': 'up,left',
+}
+
 
 def alt_tab():
     """ Switch to recent window """
@@ -12,20 +25,32 @@ def alt_tab():
     pyautogui.keyUp('alt')
 
 
-def move(direction, dash=False):
+def move(direction):
     """ Moves in the direction (up, down, left, right), total movement is 1 tile """
     pyautogui.keyDown(direction)
-    sleep(0.15)
+    sleep(0.14)
     pyautogui.keyUp(direction)
 
 
 def dash(direction):
     """ Dashes in the direction (up, down, left, right), total movement is 3 tiles """
-    pyautogui.keyDown(direction)
-    sleep(0.05)
-    pyautogui.keyDown('shift')
-    pyautogui.keyUp(direction)
-    pyautogui.keyUp('shift')
+    if len(direction) == 1:  # N, S, E, W
+        pyautogui.keyDown(directions[direction])
+        sleep(0.15)
+        # pyautogui.keyDown('shift')
+        pyautogui.press('shift')
+        pyautogui.keyUp(directions[direction])
+        # pyautogui.keyUp('shift')
+    else:  # NE, SE, SW, NW
+        d1, d2 = directions[direction].split(',')
+        pyautogui.keyDown(d1)
+        pyautogui.keyDown(d2)
+        sleep(0.2)
+        pyautogui.press('shift')
+        pyautogui.keyUp(d2)
+        pyautogui.keyUp(d1)
+        pyautogui.keyUp('shift')
+
 
 def pickup_drop():
     """ Pick up/drop item """
@@ -39,9 +64,10 @@ def chop_throw():
 
 if __name__ == '__main__':
     alt_tab()
-    for _ in range(10):
-        pickup_drop()
-        dash('left')
-        chop_throw()
-        dash('right')
+    sleep(1)
+    for _ in range(5):
+        dash('NW')
+        sleep(1)
+        dash('SE')
+        sleep(1)
     alt_tab()
